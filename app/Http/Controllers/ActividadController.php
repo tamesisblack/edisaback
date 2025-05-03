35709748aa2499715a8acdfd926750b0d7bb29e9
tamesisblack <80523426+tamesisblack@users.noreply.github.com>
2025-05-02 19:28:46 -0500

    changes

--- app/Http/Controllers/ActividadController.php
+++ app/Http/Controllers/ActividadController.php
@@ -19,32 +19,31 @@ class ActividadController extends Controller
     public function index(Request $request)
     {
 
-    
         if(!empty($request->id)){
             // $actividades = DB::SELECT("SELECT * FROM actividad WHERE libro_idlibro = ? AND estado='1'",[$request->id]);
             $actividades= DB::SELECT("SELECT t.nombre_tema, a.*, t.id as idtema ,ul.nombre_unidad, ul.id_unidad_libro , ul.unidad, lib.url as weblibro, lib.nombre as nombrelibro, concat(ul.unidad, ' - ', ul.nombre_unidad) as label
             FROM temas t, actividad a,  unidades_libros ul, libro lib
             WHERE t.id_asignatura = $request->id
             and t.id_asignatura = lib.idlibro
-            and a.id_tema = t.id 
+            and a.id_tema = t.id
             and ul.id_unidad_libro = t.id_unidad
             ");
 
-        
+
             return $actividades;
         }
-         
 
 
-       
+
+
     }
 
 
 
-    
+
     public function store(Request $request)
     {
-        
+
 
         if( $request->idactividad ){
             $actividad = Actividad::find($request->idactividad);
@@ -52,7 +51,7 @@ class ActividadController extends Controller
             $actividad = new Actividad();
         }
 
-     
+
         $actividad->id_tema = $request->id_tema;
         $actividad->url = $request->url;
         $actividad->page = $request->page;
@@ -69,11 +68,11 @@ class ActividadController extends Controller
 
         //verificar si ya ha sido la actividad en el curso
         $verificar = DB::select("SELECT *
-        FROM tarea 
+        FROM tarea
         WHERE curso_idcurso = $request->curso_id
         AND actividad_idactividad =  $request->actividad_id
         AND tipo ='1'
-        
+
         ");
 
         if(!empty($verificar)){
@@ -96,10 +95,10 @@ class ActividadController extends Controller
                 return ["status" => "1", "message" => "Se guardo correctamente"];
             }else{
                 return ["status" => "0", "message" => "No se pudo guardar"];
-            }     
+            }
         }
-        
-       
+
+
 
 
 
@@ -111,20 +110,20 @@ class ActividadController extends Controller
 
         $actividades = DB::SELECT("SELECT ac.* , a.url, a.page, a.nombre
         FROM tarea ac
-        LEFT JOIN actividad a ON ac.archivo_idarchivo  = a.idactividad 
-        WHERE ac.curso_idcurso  = ? 
+        LEFT JOIN actividad a ON ac.archivo_idarchivo  = a.idactividad
+        WHERE ac.curso_idcurso  = ?
         AND ac.estado = '1' ",[$request->idcurso]
-         
+
         );
 
         $actividades = DB::SELECT("SELECT ac.* , a.url, a.page, a.nombre
         FROM tarea ac
-        LEFT JOIN actividad a ON ac.actividad_idactividad  = a.idactividad 
-        WHERE ac.curso_idcurso  = ? 
-        AND ac.estado = '1' 
+        LEFT JOIN actividad a ON ac.actividad_idactividad  = a.idactividad
+        WHERE ac.curso_idcurso  = ?
+        AND ac.estado = '1'
         AND ac.tipo = '1'
         ",[$request->idcurso]
-         
+
         );
 
 
@@ -133,9 +132,9 @@ class ActividadController extends Controller
 
     public function actividadEstudiantePendiente(Request $request){
         $data=array();
-        $fechaActual  = date('Y/m/d'); 
-        $tarea = DB::SELECT("SELECT * FROM tarea join actividad on actividad.idactividad = tarea.actividad_idactividad 
-         WHERE tarea.curso_idcurso  = ? AND tarea.estado = '1'  AND tarea.tipo = '1' 
+        $fechaActual  = date('Y/m/d');
+        $tarea = DB::SELECT("SELECT * FROM tarea join actividad on actividad.idactividad = tarea.actividad_idactividad
+         WHERE tarea.curso_idcurso  = ? AND tarea.estado = '1'  AND tarea.tipo = '1'
          AND tarea.fecha_fin >= '$fechaActual'
          ",[$request->idcurso]
         );
@@ -151,14 +150,14 @@ class ActividadController extends Controller
     }
 
     public function ActividadEstudianteRealizada(Request $request){
-    
+
 
         if($request->docente){
-            
+
             $actividadEstudiante = DB::select("SELECT t.* ,r.res_actividad_id ,r.year,r.mes, r.url ,r.descripcion,r.nota,r.comentario ,CONCAT(u.nombre, ' ', u.apellido) as estudiante, u.cedula
             FROM tarea t, respuesta_actividad r , usuario u
             WHERE  r.actividad_id = t.idtarea
-            AND  r.usuario_idusuario = u.idusuario 
+            AND  r.usuario_idusuario = u.idusuario
             AND  t.curso_idcurso = $request->idcurso
             AND   t.estado = '1'
             AND   tipo = '1'
@@ -166,7 +165,7 @@ class ActividadController extends Controller
             ");
 
             return $actividadEstudiante;
-            
+
         }
         else{
             $data=array();
@@ -180,7 +179,7 @@ class ActividadController extends Controller
             }
             return $data;
         }
-      
+
     }
 
     public function editarCalificacionActividad(Request $request){
@@ -199,7 +198,7 @@ class ActividadController extends Controller
         }else{
             return ["status"=> "0","message" => "Ocurrio un error al guardar"];
         }
-  
+
     }
 
     public function eliminarActividadEstudiante(Request $request){
@@ -208,17 +207,17 @@ class ActividadController extends Controller
         $verificarsiExiste = DB::select("SELECT * FROM respuesta_actividad WHERE res_actividad_id = $request->res_actividad_id");
 
         if(!empty($verificarsiExiste)){
-            
+
             $year = $request->year;
             $mes  = $request->mes;
             $archivo  = $request->url;
-                        
+
             if(file_exists('archivos/tareas/'.$year.'/'.$mes.'/'.$archivo) ){
                 unlink('archivos/tareas/'.$year.'/'.$mes.'/'.$archivo);
-                
 
-            }   
-           
+
+            }
+
             $archivo = RespuestaActividad::findOrFail($request->res_actividad_id);
             $archivo->delete();
 
@@ -232,10 +231,10 @@ class ActividadController extends Controller
     {
         $temas = DB::SELECT("SELECT t.nombre_tema, t.id AS id, t.nombre_tema AS label, t.id_asignatura, t.unidad, l.nombre
         FROM temas t, libro l
-        WHERE t.id_asignatura = l.idlibro 
-        AND t.id_unidad = $request->unidad 
+        WHERE t.id_asignatura = l.idlibro
+        AND t.id_unidad = $request->unidad
         AND t.id_asignatura = $request->asignatura
-        AND t.estado = 1 
+        AND t.estado = 1
         ORDER BY  t.nombre_tema + 0 ASC
         ");
 
@@ -245,8 +244,8 @@ class ActividadController extends Controller
     //api:get>>/api/actividadEliminar?
     public function actividadEliminar(Request $request)
     {
-        
-      
+
+
         $actividad = DB::table('actividad')
             ->where('idactividad', $request->idactividad)
             ->update(['estado' => $request->estado]);
@@ -255,11 +254,11 @@ class ActividadController extends Controller
 
     public function actividades_libros_unidad($id_unidad)
     {
-        $actividades = DB::SELECT('SELECT * FROM 
-        actividad aa, temas t 
-        WHERE aa.id_tema = t.id 
+        $actividades = DB::SELECT('SELECT * FROM
+        actividad aa, temas t
+        WHERE aa.id_tema = t.id
         AND t.id_unidad = ?',[$id_unidad]
-        
+
         );
 
         return $actividades;
